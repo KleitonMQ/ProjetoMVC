8409f3a93e32dec898a8be6ffa4d1814bd448629
Kleiton MQ <kleiton.x5@gmail.com>
2022-10-06 22:45:27 -0300

    conclusao da agenda.

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -28,14 +28,62 @@ namespace ProjetoMVC.Controllers
         [HttpPost]
         public IActionResult Criar(Contato contato)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _context.Contatos.Add(contato);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-           return View(contato);
-           
+            return View(contato);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null) return NotFound();
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+            
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato ==null) return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato ==null) return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+            
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
